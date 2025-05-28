ab222f4461513b000e29c473acd021ddb3886c5d
YasirHalifi <yasirhalifi@gmail.com>
2025-05-29 01:10:50 +0200

    Update MiVector.java

--- MiVector.java
+++ MiVector.java
@@ -7,46 +7,66 @@
  */
 
 import java.util.Scanner;
+
 public class MiVector {
-        private int[] v;
-        private int tam;
-        public MiVector(){
-            tam=3;
-            v = new int [tam];
+    private int[] v;
+    private int tam;
+
+    /**
+     * Constructor que inicializa el array con tamaño fijo de 3.
+     */
+    public MiVector() {
+        tam = 3;
+        v = new int[tam];
+    }
+
+    /**
+     * Comprueba si el array está vacío (tamaño igual a 0).
+     * @return true si el array está vacío, false en caso contrario.
+     */
+    public boolean arrayVacio() {
+        if (tam == 0) {
+            return true;
         }
-        public boolean arrayVacio(){
-            if (tam==0){
-                return true;
-            }
-            return false;
+        return false;
+    }
+
+    /**
+     * Permite al usuario introducir valores por teclado en el array.
+     * Solicita exactamente tantos valores como el tamaño del array.
+     * @throws java.util.InputMismatchException si se introduce un valor no entero.
+     */
+    public void valorArray() {
+        System.out.println("Introduce " + tam + " valores:");
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < tam; i++) {
+            v[i] = scanner.nextInt();
         }
-        public void valorArray(){
-            System.out.println("Introduce "+tam+"valores:");
-            Scanner scanner = new Scanner(System.in);
-            for (int i=0; i<tam;i++){
-            v[i]= scanner.nextInt();
+    }
 
-
-
-
-
-            }
-
+    /**
+     * Muestra por pantalla todos los valores del array.
+     */
+    public void muestraArray() {
+        for (int i = 0; i < tam; i++) {
+            System.out.println(v[i]);
         }
-        public void muestraArray(){
-            for (int i=0; i<tam;i++) {
-                System.out.println(v[i]);
+    }
 
-            }
-        }
-        public int getTam(){
-            return tam;
-        }
-        public void vaciaArray() {
-           do {
-               tam--;
-           }while (tam!=0);
-            }
-        }
+    /**
+     * Devuelve el tamaño del array.
+     * @return número de elementos del array.
+     */
+    public int getTam() {
+        return tam;
+    }
 
-
+    /**
+     * Vacía el array reduciendo su tamaño a cero.
+     */
+    public void vaciaArray() {
+        do {
+            tam--;
+        } while (tam != 0);
+    }
+}
